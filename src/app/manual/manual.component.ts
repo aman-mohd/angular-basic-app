7b7b387c744803c1d7d7cd1ff6cb536047169917
aman-mohd <amanproff@gmail.com>
2024-01-26 20:57:25 +0530

    feat: created a component manually

---
+++ src/app/manual/manual.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-manual',
+  standalone: true,
+  template: '<h2>I am a manual component</h2>',
+})
+
+export class ManualComponent{
+
+}
